bc850370e1d6b14c4bbfeb5c392a52125a85f4b5
Jesse Mazzie <jessemazzie1996@gmail.com>
2022-04-26 12:35:10 -0400

    added ping function

--- server-status/main.py
+++ server-status/main.py
@@ -1,16 +1,21 @@
 import socket
+import os
 
-def poll_tcp(host, port):
+def poll_tcp(hostname, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((host, port))
+        sock.connect((hostname, port))
         return True
     except Exception as e:
         print(e)
         return False
 
+def ping(hostname):
+    response = os.system("ping " + hostname)
 
-if(poll_tcp("127.0.0.1", 443)):
+    return response == 0
+
+if ping("google.com"):
     print("it works!")
 else:
     print("oh no!")
