06f2d3f4de444765388b3d148f8b8aaf6ff29415
Jesse Mazzie <jessemazzie1996@gmail.com>
2022-04-26 08:04:37 -0400

    proof of concept

--- server-status/main.py
+++ server-status/main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import socket
+
+def poll_http(host, port = 80):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+if(poll_http("127.0.0.1")):
+    print("it works!")
+else:
+    print("oh no!")
