84815107cee6c776944b4e80ab10c06a16d010e3
Jesse Mazzie <jessemazzie1996@gmail.com>
2022-04-28 01:42:03 -0400

    changed gitignore, added function to load monitors from json.

--- server-status/main.py
+++ server-status/main.py
@@ -2,7 +2,7 @@ import socket
 import os
 import ssl
 import datetime
-from xmlrpc.client import Boolean
+import json
 
 class Monitor:
     def __init__(self, hostname, port, protocol = "tcp", interval = 5) -> None:
@@ -11,7 +11,16 @@ class Monitor:
         self.protocol = protocol
         self.interval = interval
 
-def poll_tcp(hostname, port) -> Boolean:
+#Load monitors from json file 'input.json' in root of project directory.
+def load_monitors():
+    f = open('input.json')
+
+    data = json.load(f)
+
+    return data['monitors']
+
+#Returns true if a TCP socket can be successfully opened on the given host and port, false if not.
+def poll_tcp(hostname, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((hostname, port))
@@ -20,21 +29,26 @@ def poll_tcp(hostname, port) -> Boolean:
         print(e)
         return False
 
-def ping(hostname) -> Boolean:
+#Returns true if host can be successfully pinged, false if not.
+def ping(hostname):
     response = os.system("ping " + hostname)
 
     return response == 0
 
+#Returns SSL certificate expiration date.
 def get_ssl_expiry(hostname, port = 443):
     ssl_dateformat = r'%b %d %H:%M:%S %Y %Z'
     context = ssl.create_default_context()
 
+    #Configure connection, connect.
     conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname = hostname)
-
     conn.settimeout(5.0)
-
     conn.connect((hostname, port))
+
+    #Get the cert
     cert = conn.getpeercert()
 
+    #Return just date
     return datetime.datetime.strptime(cert['notAfter'], ssl_dateformat)
 
+load_monitors()
