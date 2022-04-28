f81c161bacee2bcb4cbbd9cbe839d24c77d50b6d
Jesse Mazzie <jessemazzie1996@gmail.com>
2022-04-28 02:03:26 -0400

    cleaned up my code a little bit

--- server-status/main.py
+++ server-status/main.py
@@ -1,41 +1,45 @@
+"""
+Monitors ports n stuff.
+"""
+
 import socket
 import os
 import ssl
 import datetime
 import json
 
-class Monitor:
-    def __init__(self, hostname, port, protocol = "tcp", interval = 5) -> None:
-        self.hostname = hostname
-        self.port = port
-        self.protocol = protocol
-        self.interval = interval
-
 #Load monitors from json file 'input.json' in root of project directory.
 def load_monitors():
-    f = open('input.json')
+    input_file = open('input.json')
 
-    data = json.load(f)
+    data = json.load(input_file)
+
+    input_file.close()
 
     return data['monitors']
 
-#Returns true if a TCP socket can be successfully opened on the given host and port, false if not.
+"""
+Returns true if a TCP socket can be successfully opened on the given host and port, false if not.
+"""
 def poll_tcp(hostname, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((hostname, port))
         return True
-    except Exception as e:
-        print(e)
+    except socket.error:
         return False
 
-#Returns true if host can be successfully pinged, false if not.
+"""
+Returns true if host can be successfully pinged, false if not.
+"""
 def ping(hostname):
     response = os.system("ping " + hostname)
 
     return response == 0
 
-#Returns SSL certificate expiration date.
+"""
+Returns SSL certificate expiration date.
+"""
 def get_ssl_expiry(hostname, port = 443):
     ssl_dateformat = r'%b %d %H:%M:%S %Y %Z'
     context = ssl.create_default_context()
